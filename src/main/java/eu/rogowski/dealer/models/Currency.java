bba0419589bca90f4fca886e416c62d8ba8cae82
Piotr Rogowski <weathiel@gmail.com>
2019-11-15 19:10:25 +0100

    Added Currency entity model.

---
+++ src/main/java/eu/rogowski/dealer/models/Currency.java
@@ -0,0 +1,28 @@
+package eu.rogowski.dealer.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter @Setter @NoArgsConstructor
+public class Currency {
+    @Id
+    @JoinColumn
+    private Long currencyId;
+
+    private String curr;
+
+    private Float exchange_rate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedDate
+    private Date updatedAt;
+}
