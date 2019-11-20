8078709b9531be5443a13b5e9ba9aa1a4f9b743b
Piotr Rogowski <weathiel@gmail.com>
2019-11-20 12:30:28 +0100

    Created Repositories to Role and Cars.

--- src/main/java/eu/rogowski/dealer/repositories/CarsRepository.java
+++ src/main/java/eu/rogowski/dealer/repositories/CarsRepository.java
@@ -4,6 +4,10 @@ import eu.rogowski.dealer.models.Cars;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarsRepository extends JpaRepository<Cars, Long> {
+    List<Cars> findAllByBrand(String brand);
+    List<Cars> findModelByBrand(String brand);
 }
--- src/main/java/eu/rogowski/dealer/repositories/RoleRepository.java
+++ src/main/java/eu/rogowski/dealer/repositories/RoleRepository.java
@@ -1,8 +1,9 @@
 package eu.rogowski.dealer.repositories;
 
+import eu.rogowski.dealer.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends JpaRepository<RoleRepository, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }
